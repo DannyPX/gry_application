40a733ad12cd576bcdf32cd3b2a1b204eb5d8519
Vũ,Danny D <d.vu@student.fontys.nl>
2020-12-04 11:33:40 +0100

    API endpoints added to call back-end

--- src/store/modules/goods.js
+++ src/store/modules/goods.js
@@ -1,17 +1,27 @@
-//import api from "../api/api";
+import api from "@/api/api";
 
 export default {
   namespaced: true,
   state: {
-
+    goods: []
   },
   mutations: {
-
+    SET_GOODS(state, data) {
+      state.goods = data;
+    }
   },
   actions: {
-
+    loadGoods({ commit }) {
+      api.get("/goods/money").then(res => {
+        if(res.status === 200) {
+          commit("SET_GOODS", res.data)
+        }
+      });
+    }
   },
   getters: {
-
+    goods: state => {
+      return state.goods;
+    }
   }
 };
