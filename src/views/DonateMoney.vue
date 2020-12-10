0ec7af4454477449090a3607bfcd94c1a5e55c64
JayVnl <jorrit.v@outlook.com>
2020-12-10 11:46:42 +0100

    added input for donation amount

--- src/views/DonateMoney.vue
+++ src/views/DonateMoney.vue
@@ -3,6 +3,13 @@
     <Topbar :back="true" title="Donation" />
     <div class="container">
       <DonationTitle title="Project" name="A Dreaming Child Center" />
+      <currency-input
+        class="input"
+        v-model="value"
+        currency="USD"
+        locale="en"
+      />
+      <button @click="test">klik</button>
     </div>
   </div>
 </template>
@@ -13,6 +20,16 @@ import DonationTitle from "@/components/Donation/DonationTitle.vue";
 
 export default {
   name: "Donate",
+  data() {
+    return {
+      value: 1
+    };
+  },
+  methods: {
+    test() {
+      console.log(this.value.toFixed(2));
+    }
+  },
   components: {
     Topbar,
     DonationTitle
@@ -27,5 +44,14 @@ export default {
   flex-flow: column nowrap;
   height: calc(100vh - 150px);
   justify-content: center;
+  align-items: center;
+}
+
+.input {
+  margin-top: 50px;
+  height: 30px;
+  font-size: 1.2rem;
+  border: none;
+  text-align: center;
 }
 </style>
