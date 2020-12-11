d0bba47cc011f6c48920ab44fde82c6fdb38b930
Vũ,Danny D <d.vu@student.fontys.nl>
2020-12-11 15:46:01 +0100

    Thanks page

--- src/views/DonateComplete.vue
+++ src/views/DonateComplete.vue
@@ -1,20 +1,64 @@
 <template>
-  <div>
-    <h1>This is the donate thanks page</h1>
-    <Navbar />
+  <div class="container">
+    <img src="./../assets/thanks.jpg" alt=""/>
+    <br />
+    <h1>Thank you {{ name }}!</h1>
+    <br />
+    <p>Your donation is extremely important to our cause and we would like to thank you for your generosity. Your gift will help make dreams come true.</p>
+    <br />
+    <p> Check our newsfeed to see how all our projects are progressing.</p>
+    <div class="buttons">
+      <div class="button">
+        <Button name="Home" :inner="true" @clicked="btnClicked" />
+      </div>
+      <div class="button">
+        <Button name="Share" @clicked="btnClicked" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/Universal/Navbar.vue";
+import Button from '../components/Universal/Button.vue';
 
 export default {
+  components: { Button },
   name: "DonateComplete",
-  components: {
-    Navbar
-  },
   props: {
     name: String
+  },
+  methods: {
+    btnClicked(e) {
+      switch (e) {
+        case "Home":
+          this.$router.push("/");
+          break;
+        case "Share":
+            // TODO: Sharing
+          break;
+      }
+    }
   }
 };
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+  display: flex;
+  flex-flow: column nowrap;
+  height: calc(100vh - 50px);
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
+
+.buttons {
+  display: flex;
+}
+
+.button {
+  padding: 20px;
+}
+</style>
