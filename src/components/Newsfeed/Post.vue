8f291c86d9358e0102cfad3b1685e7c8c0b4d791
Dimitri <dimitri1997NL@hotmail.nl>
2020-12-17 14:48:56 +0100

    Twitter feed met limiet 15, + probeersel eigen post voor insta en videoplayer

---
+++ src/components/Newsfeed/Post.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="vuegram-post">
+    <div class="header level">
+      <div class="level-left">
+        <figure class="avatar">
+          <img class="avatar" src="@/assets/project1.jpg" />
+        </figure>
+        <i class="fas fa-heart mr-3"></i>
+        <span class="username">{{post.username}}</span>
+
+      </div>
+    </div>
+    <div class="content">
+      <p class="caption"> {{post.caption}}</p>
+    </div>
+    <div class="img"
+         :style="'background-image: ' + 'url(' + post.userImage + ');'">
+    </div>
+    <div class="socialbuttons">
+    <img src="../../assets/share.svg" />
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Post",
+  props: {
+    post: Object,
+  },
+  components: {
+
+  }
+
+};
+</script>
+
+<style scoped>
+.vuegram-post {
+  vertical-align: middle;
+  position: relative;
+  justify-content: center;
+  margin: 0 auto;
+  width: 86vw;
+  max-width: 320px;
+  height: auto;
+  max-height: 490px;
+  background: #fff;
+  border-radius: 24px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.16);
+  display: flex;
+  flex-flow: column nowrap;
+  margin-top: 10px;
+
+}
+.username{
+  font-weight: 600;
+  line-height: 80px;
+  margin-left: 10px;
+
+}
+.level-left{
+  text-align: left;
+}
+.socialbuttons{
+  text-align: right;
+  margin-right: 10px;
+}
+.img {
+  object-fit: contain;
+  width: auto;
+  height: 200px;
+  background-size: contain;
+  margin:5px;
+  border-radius: 15px;
+  background-position: center;
+}
+.avatar{
+  margin:5px;
+  max-width: 64px;
+  height: 64px;
+  position: relative;
+  float: left;
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+}
+.content{
+  margin: 10px;
+}
+</style>
--- src/views/Newsfeed.vue
+++ src/views/Newsfeed.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
-    <h1>This is the newsfeed page</h1>
+    <Topbar :back="true" title="Newsfeed" />
+
+    <Twitter></Twitter>
+    <!--<post v-for="post in posts"
+          :post="post"
+          :key="posts.indexOf(post)">
+    </post>-->
     <Navbar />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Universal/Navbar.vue";
+import Topbar from "@/components/Universal/Topbar.vue";
+/*import Post from "@/components/Newsfeed/Post.vue";*/
+import Twitter from "@/components/Newsfeed/Twitter.vue";
 
 export default {
   name: "Newsfeed",
   components: {
-    Navbar
-  }
+    Navbar,
+    Topbar,
+    Twitter
+  },
+  data() {
+    return {
+      posts: [
+        {
+          username: "Global Radiant Youth",
+          userImage: require("../assets/project1.jpg"),
+          postImage:require("../assets/project3.jpg"),
+          caption: "Gakire, a writer, social entrepreneur and public speaker, grew up in the remote village of in Ruhango District."
+        },
+        {
+          username: "Global Radiant Youth",
+          userImage: require("../assets/project3.jpg"),
+          postImage:require("../assets/project3.jpg"),
+          caption: "Caption test"
+        }
+      ]
+    };
+  },
 };
 </script>
